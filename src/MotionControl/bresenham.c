4956721a85f39be2f4b499b27a2834c75f15d397
Hadis <hadisemousavi8223@gmail.com>
2025-08-02 15:28:52 +0330

    Update bresenham.c

--- src/MotionControl/bresenham.c
+++ src/MotionControl/bresenham.c
@@ -10,6 +10,24 @@ extern uint32_t Target_Delay;
 extern uint32_t Start_Delay;
 extern enum { STATE_IDLE, STATE_PREPARING, STATE_ACCEL, STATE_CRUISE, STATE_DEACCEL, STATE_DONE } motion_state;
 
+void SeT_Mototrspeed(uint32_t stepindex, uint8_t op) {
+    uint32_t current_delay = 0;
+
+    switch(op) {
+        case 0: // plus
+            current_delay = Target_Delay + (Target_Delay - Start_Delay) * sqrt(1.0 - ((float)stepindex / first_phstep_x));
+            break;
+        case 1: // NULL
+            current_delay = Target_Delay;
+            break;
+        case 2: // minus
+            current_delay = Target_Delay + (Target_Delay - Start_Delay) * sqrt((float)stepindex / third_phstep_x);
+            break;
+    }
+
+    TIM2->ARR = Delay_to_counter(current_delay);
+}
+
 void Update_Delay(uint32_t index) {
     if (update_needed) {
         if (index > 0 && index <= first_phstep_x) {
