ccbec482b92f000bbb43e85bc78f9b05021b2c1f
Hadis <hadisemousavi8223@gmail.com>
2025-08-04 13:58:54 +0330

    Update timer.c

--- src/Drivers/timer.c
+++ src/Drivers/timer.c
@@ -8,6 +8,7 @@ void TIM2_Init(void) {
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
     TIM2->PSC = Prescaler - 1;
     TIM2->ARR = 1000;
+    TIM2->CR1 |= TIM_CR1_ARPE; // preload
     TIM2->CR1 |= TIM_CR1_CEN;
     TIM2->DIER |= TIM_DIER_CC1IE;
     NVIC_EnableIRQ(TIM2_IRQn);
@@ -17,6 +18,7 @@ void TIM3_Init(void) {
     RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
     TIM3->PSC = Prescaler - 1;
     TIM3->ARR = 1000;
+    TIM3->CR1 |= TIM_CR1_ARPE; // preload
     TIM3->CR1 |= TIM_CR1_CEN;
     TIM3->DIER |= TIM_DIER_CC1IE;
     NVIC_EnableIRQ(TIM3_IRQn);
