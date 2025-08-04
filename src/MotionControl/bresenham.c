6eb0528ca466f2857a4d6b03976ea14a08c42471
Hadis <hadisemousavi8223@gmail.com>
2025-08-04 13:47:45 +0330

    Update bresenham.c

--- src/MotionControl/bresenham.c
+++ src/MotionControl/bresenham.c
@@ -7,38 +7,68 @@
 
 extern bool update_needed;
 extern uint32_t Target_Delay;
-extern uint32_t Start_Delay;
+extern uint32_t Start_Delay , current_delay;
 extern enum { STATE_IDLE, STATE_PREPARING, STATE_ACCEL, STATE_CRUISE, STATE_DEACCEL, STATE_DONE } motion_state;
 
-void SeT_Mototrspeed(uint32_t stepindex, uint8_t op) {
-    uint32_t current_delay = 0;
+void Calc_StepDelay(uint32_t stepindex, uint8_t op) {
+uint32_t current_delay = 0;
 
-    switch(op) {
-        case 0: // plus
-            current_delay = Target_Delay + (Target_Delay - Start_Delay) * sqrt(1.0 - ((float)stepindex / first_phstep_x));
-            break;
-        case 1: // NULL
-            current_delay = Target_Delay;
-            break;
-        case 2: // minus
-            current_delay = Target_Delay + (Target_Delay - Start_Delay) * sqrt((float)stepindex / third_phstep_x);
-            break;
-    }
+switch(op) {  
+    case 0: // accel  
+        current_delay = Target_Delay + (Target_Delay - Start_Delay) *   
+                        sqrtf(1.0f - ((float)stepindex / first_phstep_z));  
+        break;  
+    case 1: // cruise  
+        current_delay = Target_Delay;  
+        break;  
+    case 2: // decel  
+        current_delay = Target_Delay + (Target_Delay - Start_Delay) *  
+                        sqrtf((float)stepindex / third_phstep_z);  
+        break;  
+}  
 
-    TIM2->ARR = Delay_to_counter(current_delay);
+return current_delay ;
+
+
 }
 
-void Update_Delay(uint32_t index) {
-    if (update_needed) {
-        if (index > 0 && index <= first_phstep_x) {
-            SeT_Mototrspeed(index, 0);
-        } else if (index > first_phstep_x && index <= (Stepx_required * 0.6)) {
-            motion_state = STATE_CRUISE;
-        } else if (index > (Stepx_required * 0.6) && index <= Stepx_required) {
-            SeT_Mototrspeed(index, 2);
-        } else {
-            motion_state = STATE_DONE;
-        }
-        update_needed = false;
-    }
+// ---- Motion Update (call in while(1)) ----
+void Update_Motion(void) {
+if (!update_needed) return;
+update_needed = false;
+
+switch(motion_state) {  
+    case STATE_ACCEL:  
+        if (step_index <= first_phstep_z) {  
+            TIM1->ARR = Delay_to_counter(Calc_StepDelay(step_index, 0));  
+        } else {  
+            motion_state = STATE_CRUISE;  
+        }  
+        break;  
+
+    case STATE_CRUISE:  
+        if (step_index <= Stepz_required * 0.6) {  
+            TIM2->ARR = Delay_to_counter(Calc_StepDelay(step_index, 1));  
+        } else {  
+            motion_state = STATE_DEACCEL;  
+        }  
+        break;  
+
+    case STATE_DEACCEL:  
+        if (step_index <= Stepz_required) {  
+            TIM2->ARR = Delay_to_counter(Calc_StepDelay(step_index, 2));  
+        } else {  
+            motion_state = STATE_DONE;  
+               }  
+        break;  
+
+ 
+    case STATE_DONE:
+          return ; 
+          break;
+
+    default:  
+        break;  
+}
+
 }
