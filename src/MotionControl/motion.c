d07299b0aee19bb90ba92ea82b1a0422bb2777d6
Hadis <hadisemousavi8223@gmail.com>
2025-08-06 12:59:19 +0330

    Update motion.c

--- src/MotionControl/motion.c
+++ src/MotionControl/motion.c
@@ -7,6 +7,7 @@
 #include "stepgen.h"
 #include "math_utils.h"
 #include "config.h"
+#include "error_manager.h"
 
 static enum {
     STATE_ACCEL,
@@ -22,19 +23,33 @@ bool x_is_primary;
 extern uint32_t step_index;
 
 void Move_to(int x2, int y2, int Feedrate) {
-      if (motion_state != STATE_IDLE) return;
-            dx = abs(x2 - x1);
-            dy = abs(y2 - y1);
-            sx = (x2 > x1) ? 1 : -1;
-            sy = (y2 > y1) ? 1 : -1;
-            err = 2 * dy - dx;
-            Target_Delay = feedrate_to_delay(Feedrate);
-            step_index = 0;
-            Stepx_required_calculate(dx);
-            Stepy_required_calculate(dy);
-            x_is_primary = (dx > dy);
-            motion_state = STATE_ACCEL;
-            
+    if (motion_state != STATE_IDLE)
+        return;
 
-  
+    if (x2 < PARAM_MIN  x2 > PARAM_MAX  y2 < PARAM_MIN || y2 > PARAM_MAX) {
+        ErrorHandler(ERR_OUT_OF_BOUND);
+        return;
+    }
+
+    if (Feedrate <= 0 || Feedrate > MAX_FEEDRATE) {
+        ErrorHandler(ERR_INVALID_FEEDRATE
+   );
+        return;
+    }
+
+    if (x1 == x2 && y1 == y2)
+        return; // مقصد با مبدا یکسان است
+
+    dx = abs(x2 - x1);
+    dy = abs(y2 - y1);
+    sx = (x2 > x1) ? 1 : -1;
+    sy = (y2 > y1) ? 1 : -1;
+    err = 2 * dy - dx;
+    Target_Delay = feedrate_to_delay(Feedrate);
+
+    Stepx_required_calculate(dx);
+    Stepy_required_calculate(dy);
+    x_is_primary = (dx > dy);
+
+    motion_state = STATE_ACCEL;  // فقط زمانی که همه چیز OK بود
 }
